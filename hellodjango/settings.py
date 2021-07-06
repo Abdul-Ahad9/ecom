5637755b64b75451ab520eb972898b2ea888f3a4
Abdul-Ahad9 <38209766+Abdul-Ahad9@users.noreply.github.com>
2021-07-06 16:46:57 +0500

    Update settings.py

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATIC_URL = '/static/'
 
 # Managing media
